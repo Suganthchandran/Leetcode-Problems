685b6e91da04341af69b97d1495a421097473716
Suganth <144556544+Suganthchandran@users.noreply.github.com>
2025-01-26 11:36:32 +0530

    Time: 6 ms (82.23%), Space: 56.7 MB (51.67%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        ArrayList<Integer> a = new ArrayList<>();
-        int n = nums.length;
-
+        List<Integer> a = new ArrayList<>();
         int i=0;
-        while(i<n) {
-            int correct = nums[i]-1;
-            if(nums[i] != nums[correct]) {
-                swap(nums,i,correct);
+
+        while(i < nums.length) {
+            if(nums[i] != nums[nums[i]-1]) {
+                Swap(nums,i,nums[i]-1);
             }
             else {
                 i++;
             }
         }
 
-        for(int j=0;j<n;j++) {
-            if(j!=nums[j]-1) {
+        for(int j=0;j<nums.length;j++) {
+            if(nums[j] != j+1) {
                 a.add(j+1);
             }
         }
@@ -23,7 +21,7 @@ class Solution {
         return a;
     }
 
-    public void swap(int[] nums,int a,int b) {
+    public void Swap(int[] nums,int a, int b) {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
