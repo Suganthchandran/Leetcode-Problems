b7dad71bd5901b269bb38622c257c1ff4f98b61c
Suganth <144556544+Suganthchandran@users.noreply.github.com>
2025-03-18 23:37:10 +0530

    Time: 14 ms (18%), Space: 44.5 MB (67.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,12 +4,12 @@ class Solution {
         for(int i=0;i<9;i++) {
             for(int j=0;j<9;j++) {
                 char num = board[i][j];
-                if(num != '.') {
-                    if(!set.add(num+"at row"+i) ||
-                        !set.add(num+"at col"+j) ||
-                        !set.add(num+"at box"+i/3+"-"+j/3)) {
-                            return false;
-                        }
+                if(num!='.') {
+                    if(!set.add(num+" at row - "+i) ||
+                       !set.add(num+" at col - "+j) ||
+                       !set.add(num+" at box - "+i/3+" and "+j/3)) {
+                        return false;
+                       }
                 }
             }
         }
